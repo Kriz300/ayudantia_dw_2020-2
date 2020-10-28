5a7b333f692412e2795411b01cb4390b2582e4d6
Kriz300 <christian.munoz1@mail.udp.cl>
2020-10-28 17:37:41 -0300

    28-10

--- ubuntu/mongo.php
+++ ubuntu/mongo.php
@@ -1,7 +1,7 @@
 <?php
     try {
         require '../vendor/autoload.php';
-        $client = new MongoDB\Client('Los datos para su server.');
+        $client = new MongoDB\Client('Los datos del server. En su caso "mongodb://localhost:27017"');
     } catch (\Throwable $th) {
         alert("Cago BD :'c");
     }
